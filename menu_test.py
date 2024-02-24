6da8284afa2dd7e996c3c2e8d4b8d97b62de5f50
cipherer49 <ary.kas.rt20@dypatil.edu>
2024-02-24 17:58:43 +0530

    made menu python file and written button logic

--- menu_test.py
+++ menu_test.py
@@ -4,7 +4,23 @@ class Module_menu(QtWidgets.QMainWindow):
 
     def __init__(self):
         super().__init__()
-        uic.loadUi('menu.ui',self)
+        uic.loadUi('menu_design.ui',self)
+
+        #calling the exp-tracker button
+        self.exp_track_btn.clicked.connect(self.pop_easy_exp_tracker)
+        #closing the menu screen when this button is press
+        self.exp_track_btn.clicked.connect(self.close)
+
+    #writing code to connect  Easy Expense Tracker
+    def pop_easy_exp_tracker(self):
+        #first importing expense tracker file
+        from  expense_tracker import Expense_tracker
+        #then creating an instance to open window
+        self.exp_track = Expense_tracker()
+        self.exp_track.show()
+        #closing the Module_menu window in button
+        
+
     
 
 
